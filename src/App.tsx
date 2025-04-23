3744003421e7b1f93e954f7217b7106c258cc2cc
sharifabee <sharifabee.alur@gmail.com>
2025-04-23 10:47:22 +0530

    added movies table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import MyTable from './drk-components/MyTable'
+import MoviesTable from './drk-components/MoviesTable'
 
 export default function App() {
   return (
     <div>
 
-<MyTable />
+<MoviesTable />
 
 
 
---
+++ src/drk-components/MoviesTable.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function MoviesTable() {
+
+
+
+
+  return (
+    <div>
+
+<table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Year</th>
+      <th scope="col">RunTime</th>
+      <th scope="col">Poster</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+   
+  </tbody>
+</table>
+
+
+
+
+    </div>
+  )
+}
